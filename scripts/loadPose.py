657c23386176e848ec4d76a4b970c7578b6b6835
Deepak singh <deepaks.ec.16@nsit.net.in>
2021-09-15 20:05:10 +0530

    Updated load pose estimator using april tags

--- scripts/loadPose.py
+++ scripts/loadPose.py
@@ -5,6 +5,7 @@ import ros_numpy
 import numpy as np
 from cv_bridge import CvBridge
 from sensor_msgs.msg import PointCloud2, Image
+from geometry_msgs.msg import PoseStamped
 
 # declare a CvBridge object
 br = CvBridge()
@@ -19,51 +20,52 @@ def findArucoMarkers(img, markerSize = 6, totalMarkers=250, draw=True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
     arucoParam = aruco.DetectorParameters_create()
- #   arucoParam.aprilTagMinWhiteBlackDiff = 30 
- #   arucoParam.aprilTagMaxLineFitMse = 20                          
- #   arucoParam.maxErroneousBitsInBorderRate = 0.35                 
- #   arucoParam.errorCorrectionRate = 1.0                             
- #   arucoParam.minMarkerPerimeterRate = 0.20                 
- #   arucoParam.maxMarkerPerimeterRate = 4     
- #   arucoParam.polygonalApproxAccuracyRate = 0.05  
     bboxs, ids, rejected = aruco.detectMarkers(gray, arucoDict, markerCorners, parameters = arucoParam)
 
-    #board = cv2.aruco.Board.create()
     bboxs, ids, rejected = aruco.detectMarkers(gray, arucoDict, markerCorners, parameters = arucoParam)
 
     img_ = img.copy()
-    
+
+    aruco.drawDetectedMarkers(img_, bboxs)
+
+    tvec  = np.zeros(3)
+
+    pX = 0
+    pY = 0
+
     if ids is not None:
-        arr = bboxs[0][0]
-        cXcenter = 0
-        cYcenter = 0
-        aruco.drawDetectedMarkers(img_, bboxs)
+        for i in range(len(bboxs)):
+            cXcenter = 0
+            cYcenter = 0
+    
+            arr = np.array(bboxs[i][0])
 
-        for i in range(arr.shape[0]):
-            cX = arr[i,0]
-            cY = arr[i,1]
-            cXcenter += cX
-            cYcenter += cY 
+            for j in range(arr.shape[0]):
+                cX = arr[j,0]
+                cY = arr[j,1]
+                cXcenter += cX
+                cYcenter += cY 
+            
+            cXcenter = int(cXcenter/4.0)
+            cYcenter = int(cYcenter/4.0)
+            
+            pX += cXcenter
+            pY += cYcenter
+
+            cv2.circle(img_, (int(cXcenter), int(cYcenter)), 16, (0,255,0), -1)
+            
+            pose = cv2.aruco.estimatePoseSingleMarkers(np.array(bboxs[i]), 0.05, K, D)
+            tvec += pose[1][0][0]
+            
             cv2.circle(img_, (int(cX), int(cY)), 8, (0,0,255), -1)
-        
-        cXcenter = int(cXcenter/4.0)
-        cYcenter = int(cYcenter/4.0)
-
-        cv2.circle(img_, (int(cXcenter), int(cYcenter)), 16, (0,255,0), -1)
-        
-        rvecs = np.zeros(3)
-        tvecs = np.zeros(3)
-
-        pose = cv2.aruco.estimatePoseSingleMarkers(np.array(bboxs), 0.05, K, D)
-        #pose = cv2.aruco.estimatePoseBoard(np.array(bboxs), np.array(ids), 0.05, K, D, rvecs, tvecs)
-        tvec = pose[1][0][0].tolist()
-        print(tvec)
-        
-        # this is the coordinate of the payload with respect to camera
-        # now use the transformation from camera to base-link to convert the pose in body-frame and publish it over TF
-        
-
+    
+        cv2.circle(img_, ( int(pX/len(bboxs)), int(pY/len(bboxs)) ), 16, (255,0,0), -1)
+    
         img_ = cv2.putText(img_, str(tvec), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA)
+
+        loadPose_camFrame = tvec/len(bboxs)
+        print(loadPose_camFrame)
+
     return img_
 
 def imgCallback(msg):
@@ -77,7 +79,7 @@ def imgCallback(msg):
     cv2.waitKey(1)
 
 def poseCallback(msg):
-
+    pass
 
 
 def genData():
