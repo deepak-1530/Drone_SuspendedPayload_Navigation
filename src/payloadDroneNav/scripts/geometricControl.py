1603c66f29a84d4cb7ffb51413c52ca7b43e97b1
Deepak singh <deepaks.ec.16@nsit.net.in>
2021-09-25 22:09:22 +0530

    Added launch files

---
+++ src/payloadDroneNav/scripts/geometricControl.py
@@ -0,0 +1,6 @@
+##############################################
+## Geometric control of quadrotors in python #
+##############################################
+
+# Input  -> trajectory -> position, orientation (take velocity, acceleration when needed)
+# Output -> wx, wy, wz, thrust
