2648e412cea86026c294de273b00b9e5eb51abee
Deepak singh <deepaks.ec.16@nsit.net.in>
2022-09-16 15:47:18 +0530

    deleted-folders

--- build/payloadDroneNav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/deepak/Drone_SuspendedPayload_Navigation/src/payloadDroneNav
-# Build directory: /home/deepak/Drone_SuspendedPayload_Navigation/build/payloadDroneNav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
